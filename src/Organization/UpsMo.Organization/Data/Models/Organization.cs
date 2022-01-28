373203aa7ea06871ec8fb7a64e739fa0fc76814b
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-01-29 00:59:27 +0300

    update organization.cs to add managers prop

--- src/Organization/UpsMo.Organization/Data/Models/Organization.cs
+++ src/Organization/UpsMo.Organization/Data/Models/Organization.cs
@@ -4,6 +4,13 @@ using UpsMo.Common.Data.Model;
 
 public class Organization : BaseEntity<Guid>
 {
+    public Organization()
+    {
+        Managers = new List<Manager>();
+    }
+
     public string? Name { get; set; }
     public int FounderUserID { get; set; }
+
+    public virtual List<Manager> Managers { get; set; }
 }
