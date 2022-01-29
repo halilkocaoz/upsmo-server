66412399c41eb485a8f7e6c907eb884140919667
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-01-29 15:52:09 +0300

    remove a repository, update datacontext

--- src/Organization/UpsMo.Organization/Data/DataContext.cs
+++ src/Organization/UpsMo.Organization/Data/DataContext.cs
@@ -4,8 +4,8 @@ namespace UpsMo.Organization.Data;
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-    public DbSet<UpsMo.Organization.Data.Models.Organization> Organizations { get; set; }
-    public DbSet<UpsMo.Organization.Data.Models.Manager> Managers { get; set; }
+    public DbSet<Models.Organization> Organizations { get; set; }
+    public DbSet<Models.Manager> Managers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
